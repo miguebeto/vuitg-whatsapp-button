37bc8269930102548e5738ae0305f816739a91f6
Miguel Blanco <78479657+miguebeto@users.noreply.github.com>
2022-04-08 09:41:56 -0500

    update whatsappButton

--- react/components/whatsappButton.tsx
+++ react/components/whatsappButton.tsx
@@ -5,8 +5,8 @@ type Props = {
     logo: string,   //whatsApp.png
     phone: string,  //3058779813
     message: string,  //Estás comunicándote con VTEX University, por favor ingresa tu duda.
-    width: number,    //80px
-    height: number    //80px
+    width: number,    //45px
+    height: number    //45px
 
 }
 
@@ -14,9 +14,9 @@ const whatsappButton = ({ logo, phone, message, width, height }: Props) => {
     const formattedMessage = message.replace(/ /g, "%20")
     console.log("mi mensaje formateado es :", formattedMessage, logo);
     return <>
-        <div className='fixed botton-2 right-2 flex flexColumn'>
+        <div className='fixed botton-1 z-1 right-1 flex flexColumn'>
             <a
-                href={`https://wa.me/${phone}?text=Me%20interesa%20in%20el%20auto%20que%20vendes`}
+                href={`https://wa.me/${phone}?text=${formattedMessage}`}
                 target='_blank'
                 rel="noreferrer noopener"
             >
@@ -40,7 +40,9 @@ whatsappButton.propTypes = {
 whatsappButton.defaultProps = {
     logo: "mi-logo.png",
     phone: "3058779813",
-    message: "Estás comunicando con vtex university, por favor, ingresa tu duda"
+    message: "Estás comunicando con vtex university, por favor, ingresa tu duda",
+    width: 60,
+    height: 60
 }
 
 whatsappButton.schema = {
